dc4ec4a17991c819b8984bce9442c9e62ad8f141
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-05-31 15:24:36 +0300

    now worked

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-
-
-
 def magic_calculation(a, b, c):
-  
     if a < b:
-        return (c)
+        return c
     if c > b:
-        return (a + b)
-    return (a*b - c)
+        return a + b
+    return a * b - c
