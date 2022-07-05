3661f509c312465b9e0c6891aaea7e417bf53199
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-07-05 14:58:24 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+'''function that returns the dictionary description with simple data structure
+'''
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+def class_to_json(obj):
+    '''module class_to_json
+       returns builds a dictionary
+    '''
+    return obj.__dict__
