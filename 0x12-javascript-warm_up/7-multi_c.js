495d3f05468baf09b789d206ee9d1a7a4dc60b18
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-06 09:02:09 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let num = process.argv[2];
-
-if (!isNaN(parseInt(process.argv[2]))) {
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
-  }
+let printXt = parseInt(process.argv[2]);
+if (isNaN(printXt) || process.argv[2] === undefined) {
+  console.log('Missing number of occurences');
 } else {
-  console.log('Missing number of occurrences');
+  while (printXt > 0) {
+    console.log('C is fun');
+    printXt--;
+  }
 }
