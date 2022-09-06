a32614b7f808e41b7fb0d847c8e3527ce1250c86
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-06 09:03:03 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0, len = myArray.length; i < len; i++) {
-  console.log(myArray[i]);
+const myStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
+
+while (idx < 3) {
+  console.log(myStr[idx]);
+  idx++;
 }
