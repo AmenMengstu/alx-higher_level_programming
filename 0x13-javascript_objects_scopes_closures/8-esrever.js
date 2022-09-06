1c753ac4ec6d01e762453d7299295a8787439b4d
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-06 16:01:10 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+// function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+
+  let rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
+  }
+  return rlist;
 };
