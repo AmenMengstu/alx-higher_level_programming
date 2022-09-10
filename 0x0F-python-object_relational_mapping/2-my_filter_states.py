154f7b25fe273173d1e8f67d44bdc6bfd3104a87
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-10 11:17:06 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+takes 4 arguments: mysql username, mysql password, database name and state name searched (no argument validation needed)
+"""
 
-
+import sys
 import MySQLdb
-from sys import argv
 
-'''
-Script that lists all states from the database
-'''
-if __name__ == "__main__":
-    cont = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = cont.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE"
-            " '{:s}' ORDER BY id ASC".format(argv[4]))
-    db = cursor.fetchall()
-    for i in db:
-        if i[1] == argv[4]:
-            print(i)
+
+def state():
+    """ defining func"""
+
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, name FROM `states` where `name` LIKE '{}' ORDER BY id ASC".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    state()
