52891a3e3d858c3c1e6e22950f51a2bd54f97099
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-10 11:33:23 +0300

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-'''
-    Defines classes for tables
-'''
-from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""
+State class:
+	inherits from Base Tips
+	links to the MySQL table states
+	attributes:
+		id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+		name that represents a column of a string with maximum 128 characters and can’t be null
+"""
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-class City(Base):
-    '''
-        Creates table for cities
-    '''
-    __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False)
+Base = declarative_base()
+
+class State(Base):
+    """ defining class """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="cities")
