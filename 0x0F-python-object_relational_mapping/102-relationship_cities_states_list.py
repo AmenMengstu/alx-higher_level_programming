65464da444ff981da988bbf6ada5016dcce9cccf
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-10 11:44:40 +0300

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-
+""" Lists all City objects from the database hbtn_0e_101_usa.
+     Usage: ./102-relationship_cities_states_list.py <mysql username> /
+                                                 <mysql password> /
+                                                 <database name>
 """
-    A script that lists all states from the database hbtn_0e_0_usa
-    starting with capital letter N
-    Username, password and database names are given as user args
-"""
-
-
 import sys
-import MySQLdb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
-
-    cursor = db.cursor()
-
-    sql = """SELECT * FROM states
-          WHERE name = %s
-          ORDER BY id ASC"""
-
-    cursor.execute(sql, (sys.argv[4],))
-
-    data = cursor.fetchall()
-
-    for row in data:
-        print(row)
-
-    cursor.close()
-    db.close()
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
