26342c9cf94c0f8f4220b9f534a2e6a18b950a2e
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-10 11:40:17 +0300

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
+
 """
-takes in arguments and displays all values in the states 
-table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that is safe from MySQL injections!
-takes 4 arguments: mysql username, mysql password, database
-name and state name searched (safe from MySQL injection)
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
 """
+
 
 import sys
 import MySQLdb
 
 
-def safe_filter():
-    """ defining func """
-
-    conn = MySQLdb.connect(host='localhost', port=3306,
-                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM `states`")
-    [print(state) for state in cur.fetchall() if state[1] == sys.argv[4]]
-    cur.close()
-    conn.close()
-
-
 if __name__ == '__main__':
-    safe_filter()
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
