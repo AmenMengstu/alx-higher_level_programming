848ffe887f942560c5b6d350426d63a1fd1cc108
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-09-10 11:16:08 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:
+takes 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+Result sorted in ascending order by states.id
+"""
 
-
+import sys
 import MySQLdb
-from sys import argv
 
-'''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
-'''
-if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3])
-    cursor = con.cursor()
-    cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
-    db = cursor.fetchall()
-    for i in db:
-        print(i)
+
+def N_states():
+    """defining func"""
+
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT id, name FROM `states` WHERE name LIKE 'N%' ORDER BY id ASC")
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    N_states()
