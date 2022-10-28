ca0b15fbbe45008eaebcdaeba06b5d5d133a2bd7
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-10-28 09:47:44 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-// A script that display the status code of a GET request
-
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else console.log('code:', response && response.statusCode);
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
